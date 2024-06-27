fb7c6c08232416fd757b4d1aad2a520e293a077c
Lucas Aguilar <lucassaguilar01@gmail.com>
2024-06-26 21:53:56 -0300

    First commit

---
+++ script.js
@@ -0,0 +1,25 @@
+const numberOfPixels = 150;
+const bodyElement = document.querySelector("body");
+
+function createPixel() {
+  const pixel = document.createElement("div");
+  pixel.className = "pixel";
+
+  // Gera propriedades aleatórias
+  const top = Math.floor(Math.random() * 200); 
+  const left = Math.floor(Math.random() * -10); 
+  const delay = Math.random() * 5; 
+
+  // Aplica as propriedades
+  pixel.style.top = `${top}%`;
+  pixel.style.left = `${left}%`;
+  pixel.style.animationDelay = `${delay}s`; 
+
+  bodyElement.appendChild(pixel);
+
+  setTimeout(() => {
+    pixel.remove();
+  }, 21000); 
+}
+
+setInterval(createPixel, 100); // Cria um novo pixel a cada 500ms
